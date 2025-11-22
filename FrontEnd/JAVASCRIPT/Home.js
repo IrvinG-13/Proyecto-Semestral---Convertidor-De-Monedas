ba4b82049de5afaf51f777da15df7acfdb737897
irvin gonzalez <irvigonza13@gmail.com>
2025-11-22 16:13:21 -0500

    Pagina del Login y Logica del boton del perfil si no estas logged

--- FrontEnd/JAVASCRIPT/Home.js
+++ FrontEnd/JAVASCRIPT/Home.js
@@ -1,3 +1,24 @@
+//Cmabiar entre paginas
 function ir(page) {
-    window.location.href = `../HTML/${page}.html`;
+  const logged = localStorage.getItem("isLogged");
+
+  if (page === "Perfil" && !logged) {
+    window.location.href = "../HTML/Login.html";
+    return;
+  }
+
+  window.location.href = `../HTML/${page}.html`;
+
 }
+
+document.getElementById("btnPerfil").addEventListener("click", () => {
+  ir("Perfil");
+});
+
+//Esto es para que te abra a la pgian del login y registro, si no etas logueado
+console.log(localStorage.removeItem("isLogged"))
+
+
+
+//Pon esto si estas logueado
+//localStorage.setItem("isLogged", "True");
---
+++ FrontEnd/JAVASCRIPT/Login.js
@@ -0,0 +1,12 @@
+//Logica del login y registro para la animacion
+const contenedor = document.getElementById("contenedor");
+const btnIzquierdo = document.getElementById("btnIzquierdo");
+const btnDerecho = document.getElementById("btnDerecho");
+
+btnIzquierdo.addEventListener("click", () => {
+    contenedor.classList.add("movDerecho");
+});
+
+btnDerecho.addEventListener("click", () => {
+    contenedor.classList.remove("movDerecho");
+});
